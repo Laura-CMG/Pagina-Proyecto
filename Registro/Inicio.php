c9216bdd4434f384a1e134a9fb4c7d0494035638
JuanSebastianPW <JuanPintoW@gmail.com>
2025-05-29 20:11:16 -0500

    type docs

---
+++ Registro/Inicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <script src="..//index javas.js"></script>
+    <title>ILOE</title>
+</head>
+<body>
+    <div id="Menu">
+        <ul class="menu">
+            <li class="out-menu-menu" onclick="Menu">menu
+                <ul class="menu-desplazable">
+                    <li class="in-menu">IMAGEN</li>
+                    <li class="in-menu">NOSOTROS</li>
+                    <li class="in-menu">PRODUCTOS</li>
+                    <li class="in-menu">AYUDA</li>
+                </ul>
+            </li>
+    </div>
+</body>
+</html>
